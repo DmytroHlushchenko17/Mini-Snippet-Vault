f9d4bcfd7da9c6229dcfb87694ed118efcc41309
Dmytro Hlushchenko <dimalifecell2019@gmail.com>
2026-03-09 15:35:24 +0200

    add:folder companents

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+'use client';
+import Loader from '@/components/Loader/Loader';
+
+export default function Loading() {
+  return (
+    <main>
+      <Loader />
+      <p>Loading, please wait...</p>
+    </main>
+  );
+}
